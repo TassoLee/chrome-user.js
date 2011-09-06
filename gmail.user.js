6a6912a368f91e1e3910d61bed854a9de5c3f988
TassoLee <NexusOne.Lee@gmail.com>
2011-09-06 14:38:41 +0800

    add gmail user script

---
+++ gmail.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @match http://mail.google.com/*
+// @match https://mail.google.com/*
+// ==/UserScript==
+
+var css = " @namespace url(http://www.w3.org/1999/xhtml); ";
+
+css += " table.T1HY1.nH.iY { width:100% !important; } "; // expand mail text
+css += " table.Bs.nH.iY td.Bu:last-child { display:none; } "; // hide right bar ads
+css += " div.nH.OH0apf { display:none; } "; // ads under a open mail
+css += " div.T8uMgc { display:none; } "; // Invite a friend
+
+GM_addStyle(css);
