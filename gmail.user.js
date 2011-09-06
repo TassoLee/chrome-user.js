959a52340d7892c312d15a6d2ba2823f3a2ec62d
TassoLee <NexusOne.Lee@gmail.com>
2011-09-06 15:21:34 +0800

    modify hide invite a friend

--- gmail.user.js
+++ gmail.user.js
@@ -8,6 +8,6 @@ var css = " @namespace url(http://www.w3.org/1999/xhtml); ";
 css += " table.T1HY1.nH.iY { width:100% !important; } "; // expand mail text
 css += " table.Bs.nH.iY td.Bu:last-child { display:none; } "; // hide right bar ads
 css += " div.nH.OH0apf { display:none; } "; // ads under a open mail
-css += " div.T8uMgc { display:none; } "; // Invite a friend
+css += " div.pS.pY { display:none; } "; // Invite a friend
 
 GM_addStyle(css);
