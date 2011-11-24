55198797a8141b492580c6d8650f953c3216f687
TassoLee <NexusOne.Lee@gmail.com>
2011-11-24 22:20:40 +0800

    Google has changed the gmail page source

--- gmail.user.js
+++ gmail.user.js
@@ -7,7 +7,7 @@ var css = " @namespace url(http://www.w3.org/1999/xhtml); ";
 
 css += " table.T1HY1.nH.iY { width:100% !important; } "; // expand mail text
 css += " table.Bs.nH.iY td.Bu:last-child { display:none; } "; // hide right bar ads
-css += " div.nH.OH0apf { display:none; } "; // ads under a open mail
+css += " div.Zs { display:none; } "; // ads under a open mail
 css += " div.pS.pY { display:none; } "; // Invite a friend
 css += " div.w-asK.w-atd { display:none; } "; // Send feedback
 
