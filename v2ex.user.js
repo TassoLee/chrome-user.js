2955fa90ccc26d7dfe528bdb3082ab953a7fec2c
TassoLee <NexusOne.Lee@gmail.com>
2013-12-12 22:01:00 +0800

    remove v2ex

--- v2ex.user.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @match http://v2ex.com/*
-// @match http://www.v2ex.com/*
-// @exclude http://v2ex.com/signin
-// ==/UserScript==
-
-var i = location.pathname.indexOf("/t/") + 2;
-document.getElementsByClassName("sep20")[i].style.display = "none";
-document.getElementsByClassName("box")[i].style.display = "none";
